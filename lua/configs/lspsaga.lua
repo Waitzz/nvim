24e175418c34c4dbf335f0b822e6b53325ee7bd8
gavin.xu <gavin.xu@sigmastar.com.cn>
2024-07-08 12:13:35 +0800

    Add some keymaps.

--- lua/configs/lspsaga.lua
+++ lua/configs/lspsaga.lua
@@ -3,10 +3,29 @@ if not is_ok then
     return
 end
 
+local opts = {noremap = true, silent = true}
+vim.keymap.set("n", "<leader>c", "<cmd>Lspsaga incoming_calls<cr>", opts)
+vim.keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<cr>", opts)
+
 lspsaga.setup({
     finder = {
         keys = {
             toggle_or_open = "<enter>",
+            quit = "<esc>"
+        }
+    },
+
+    outline = {
+        layout = "float",
+        keys = {
+            toggle_or_jump = "<enter>",
+            quit = "<esc>"
+        }
+    },
+
+    callhierarchy = {
+        keys = {
+            edit = "<enter>",
             quit = "<esc>"
         }
     }
