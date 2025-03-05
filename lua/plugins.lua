51930517db4141e0a9433415d2b3647e76e48d2e
Waitzz <59369014+Waitzz@users.noreply.github.com>
2025-03-05 20:41:39 +0800

    Add markdown-preview.nvim.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -174,5 +174,15 @@ require("lazy").setup({
         config = function ()
             require("configs/illuminate")
         end
+    },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+            init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
     }
 })
