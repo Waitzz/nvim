b1cbdfc23b704e5f48dc4158b8974d8fcbad199f
Waitzz <59369014+Waitzz@users.noreply.github.com>
2025-12-24 21:16:35 +0800

    Disable noice.nvim signature and hover because they conflict with nvim-cmp and lspsaga.nvim.

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -10,6 +10,14 @@ noice.setup({
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+
+        signature = {
+            enabled = false
+        },
+
+        hover = {
+            enabled = false
         }
     },
     -- you can enable a preset for easier configuration
